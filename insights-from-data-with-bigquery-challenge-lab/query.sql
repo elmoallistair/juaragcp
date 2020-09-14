ad7915350643093df71b6cb285e704ca1fa3bb0f
adipurnamk <muhammad.adipurna.k@gmail.com>
2020-09-14 09:30:19 +0700

    updated query 10
    I'am facing issue when using previous query, the Covid dataset is not found
    
    Then the CS point me a new query

--- insights-from-data-with-bigquery-challenge-lab/query.sql
+++ insights-from-data-with-bigquery-challenge-lab/query.sql
@@ -223,10 +223,14 @@ FROM summary
 
 -- Query:
 SELECT
-    date, SUM(cumulative_confirmed) AS country_cases,
-    SUM(cumulative_deceased) AS country_deaths
-FROM `bigquery-public-data.covid19_open_data.covid19_open_data`
-WHERE date BETWEEN '2020-03-15' AND '2020-04-30' AND country_name ="United States of America"
+  date, SUM(cumulative_confirmed) AS country_cases,
+  SUM(cumulative_deceased) AS country_deaths
+FROM
+  `bigquery-public-data.covid19_open_data.covid19_open_data`
+WHERE
+  date BETWEEN '2020-03-15'
+  AND '2020-04-30'
+  AND country_name ="United States of America"
 GROUP BY date
 
 -- Expected:
