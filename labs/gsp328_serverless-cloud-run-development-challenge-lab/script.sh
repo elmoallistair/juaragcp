f032aa842b69609ae5d6c1c450a2401ebd18339a
Fikri Milano <62053304+FikriMilano@users.noreply.github.com>
2021-03-16 08:54:38 +0700

    Update script.sh
    - Delete duplicate codes
    - Add Provision the Qwiklabs environment (Prep code)
    - Overall made it easier to follow
    
    Note: Tested and success

--- labs/gsp328_serverless-cloud-run-development-challenge-lab/script.sh
+++ labs/gsp328_serverless-cloud-run-development-challenge-lab/script.sh
@@ -1,15 +1,28 @@
+# Serverless Cloud Run Development: Challenge Lab
+# https://www.qwiklabs.com/focuses/14744
+
+# Provision the Qwiklabs environment
+gcloud config set project \
+  $(gcloud projects list --format='value(PROJECT_ID)' \
+  --filter='qwiklabs-gcp')
+  
+gcloud config set run/region us-central1
+gcloud config set run/platform managed
+git clone https://github.com/rosera/pet-theory.git && cd pet-theory/lab07
+
+
 # Task 1. Enable a Public Service
 cd ~/pet-theory/lab07/unit-api-billing
 gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.1
-
+  
 gcloud run deploy public-billing-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.1 \
-    --allow-unauthenticated
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.1 \
+  --allow-unauthenticated
+    
+# If the check fails, try to check the progress from the checkpoints menu on the right
+# If still fails, keep doing it
 
-gcloud run deploy public-billing-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.1 \
-    --allow-unauthenticated
 
 # Task 2. Deploy a Frontend Service
 cd ~/pet-theory/lab07/staging-frontend-billing
@@ -17,12 +30,12 @@ gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1
 
 gcloud run deploy frontend-staging-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1 \
-    --allow-unauthenticated
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1 \
+  --allow-unauthenticated
 
-gcloud run deploy frontend-staging-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1 \
-    --allow-unauthenticated
+# If the check fails, try to check the progress from the checkpoints menu on the right
+# If still fails, keep doing it
+
 
 # Task 3. Deploy a Private Service
 cd ~/pet-theory/lab07/staging-api-billing
@@ -32,12 +45,8 @@ gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.2
 
 gcloud run deploy private-billing-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.2 \
-    --no-allow-unauthenticated
-
-gcloud run deploy private-billing-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.2 \
-    --no-allow-unauthenticated
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.2 \
+  --no-allow-unauthenticated
 
 BILLING_SERVICE=private-billing-service
 BILLING_URL=$(gcloud run services describe $BILLING_SERVICE \
@@ -45,8 +54,16 @@ BILLING_URL=$(gcloud run services describe $BILLING_SERVICE \
 
 curl -X get -H "Authorization: Bearer $(gcloud auth print-identity-token)" $BILLING_URL
 
+# If the check fails, try to check the progress from the checkpoints menu on the right
+# If still fails, keep doing it
+
+
 # Task 4. Create a Billing Service Account
 gcloud iam service-accounts create billing-service-sa --display-name "Billing Service Cloud Run"
+
+# If the check fails, try to check the progress from the checkpoints menu on the right
+# If still fails, keep doing it
+
 
 # Task 5. Deploy the Billing Service
 cd ~/pet-theory/lab07/prod-api-billing
@@ -54,12 +71,8 @@ gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/billing-prod-api:0.1
 
 gcloud run deploy billing-prod-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-prod-api:0.1 \
-    --no-allow-unauthenticated
-
-gcloud run deploy billing-prod-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-prod-api:0.1 \
-    --no-allow-unauthenticated
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-prod-api:0.1 \
+  --no-allow-unauthenticated
 
 gcloud run services add-iam-policy-binding billing-prod-service \
   --member=serviceAccount:billing-service-sa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
@@ -68,15 +81,23 @@ gcloud run services add-iam-policy-binding billing-prod-service \
 PROD_BILLING_SERVICE=private-billing-service
 
 PROD_BILLING_URL=$(gcloud run services \
-    describe $PROD_BILLING_SERVICE \
-    --format "value(status.URL)")
+  describe $PROD_BILLING_SERVICE \
+  --format "value(status.URL)")
 
 curl -X get -H "Authorization: Bearer \
-    $(gcloud auth print-identity-token)" \
-    $PROD_BILLING_URL
+  $(gcloud auth print-identity-token)" \
+  $PROD_BILLING_URL
+    
+# If the check fails, try to check the progress from the checkpoints menu on the right
+# If still fails, keep doing it
+
 
 # Task 6. Frontend Service Account
 gcloud iam service-accounts create frontend-service-sa --display-name "Billing Service Cloud Run Invoker"
+
+# If the check fails, try to check the progress from the checkpoints menu on the right
+# If still fails, keep doing it
+
 
 # Task 7. Redeploy the Frontend Service
 cd ~/pet-theory/lab07/prod-frontend-billing
@@ -84,13 +105,12 @@ gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-prod:0.1
 
 gcloud run deploy frontend-prod-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-prod:0.1 \
-    --allow-unauthenticated
-
-gcloud run deploy frontend-prod-service \
-    --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-prod:0.1 \
-    --allow-unauthenticated
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-prod:0.1 \
+  --allow-unauthenticated
 
 gcloud run services add-iam-policy-binding frontend-prod-service \
   --member=serviceAccount:frontend-service-sa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
   --role=roles/run.invoker
+  
+# If the check fails, try to check the progress from the checkpoints menu on the right
+# If still fails, keep doing it
