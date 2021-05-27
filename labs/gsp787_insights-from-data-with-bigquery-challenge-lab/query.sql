aaf89d9a14676a45caf2640b899744e186ef739d
BAKI YASWANTH REDDY <63598964+bakiyaswanth@users.noreply.github.com>
2021-05-27 22:09:14 +0530

    Update query.sql

--- labs/gsp787_insights-from-data-with-bigquery-challenge-lab/query.sql
+++ labs/gsp787_insights-from-data-with-bigquery-challenge-lab/query.sql
@@ -185,34 +185,57 @@ LIMIT 10
 -- Query 9: CDGR - Cumulative Daily Growth Rate
 -- Calculate the CDGR on May 10, 2020(Cumulative Daily Growth Rate) for France since the day the first case was reported. 
 -- The first case was reported on Jan 24, 2020.
-WITH france_cases AS (
-    SELECT
-        date,
-        SUM(cumulative_confirmed) AS total_cases
-    FROM `bigquery-public-data.covid19_open_data.covid19_open_data`
-    WHERE country_name="France" AND date IN ('2020-01-24', '2020-05-10')
-    GROUP BY date
-    ORDER BY date
-), 
-	
-summary AS (
-    SELECT
-        total_cases AS first_day_cases,
-        LEAD(total_cases) OVER(ORDER BY date) AS last_day_cases,
-        DATE_DIFF(LEAD(date) OVER(ORDER BY date),date, day) AS days_diff
-    FROM france_cases
-    LIMIT 1
+WITH
+
+  france_cases AS (
+
+  SELECT
+
+    date,
+
+    SUM(cumulative_confirmed) AS total_cases
+
+  FROM
+
+    `bigquery-public-data.covid19_open_data.covid19_open_data`
+
+  WHERE
+
+    country_name="France"
+
+    AND date IN ('2020-01-24',
+
+      '2020-05-10')
+
+  GROUP BY
+
+    date
+
+  ORDER BY
+
+    date)
+
+, summary as (
+
+SELECT
+
+  total_cases AS first_day_cases,
+
+  LEAD(total_cases) OVER(ORDER BY date) AS last_day_cases,
+
+  DATE_DIFF(LEAD(date) OVER(ORDER BY date),date, day) AS days_diff
+
+FROM
+
+  france_cases
+
+LIMIT 1
+
 )
 
-SELECT 
-    first_day_cases, 
-    last_day_cases, 
-    days_diff, 
-    POWER(last_day_cases/first_day_cases,1/days_diff)-1 as cdgr
-    -- Note: if you received null in column last_day_cases, days_diff and cdgr
-    -- Then try use POW() instead of POWER()
-    -- POW(last_day_cases/first_day_cases,1/days_diff)-1 as cdgr
-FROM summary
+select first_day_cases, last_day_cases, days_diff, POW((last_day_cases/first_day_cases),(1/days_diff))-1 as cdgr
+
+from summary
 
 -- Expected
 -- Row      first_day_cases      last_day_cases     days_diff 	    cdgr 	
