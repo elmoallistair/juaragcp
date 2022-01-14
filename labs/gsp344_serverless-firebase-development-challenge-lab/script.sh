1601ea0cfadbdad048cbb4f40213720d0e47264a
Gabril Hozanna <gabrielhozana97@gmail.com>
2022-01-14 15:17:36 +0800

    Update script.sh

--- labs/gsp344_serverless-firebase-development-challenge-lab/script.sh
+++ labs/gsp344_serverless-firebase-development-challenge-lab/script.sh
@@ -13,15 +13,15 @@ node index.js netflix_titles_original.csv
 cd ~/pet-theory/lab06/firebase-rest-api/solution-01
 npm install
 gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.1
-gcloud beta run deploy netflix-dataset-service --image gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.1 --allow-unauthenticated
-# Choose 1 and us-central1
+gcloud beta run deploy netflix-dataset-service-574 --image gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.1 --max-instances=1 --allow-unauthenticated 
+# Choose 23 and us-central1
 
 # 4. Cloud Build Rest API Production
 cd ~/pet-theory/lab06/firebase-rest-api/solution-02
 npm install
 gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.2
-gcloud beta run deploy netflix-dataset-service --image gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.2 --allow-unauthenticated
-# go to cloud run and click netflix-dataset-service then copy the url
+gcloud beta run deploy netflix-dataset-service-574 --image gcr.io/$GOOGLE_CLOUD_PROJECT/rest-api:0.2 --max-instances=1 --allow-unauthenticated
+# go to cloud run and click netflix-dataset-service-574 then copy the url
 SERVICE_URL=<copy url from your netflix-dataset-service>
 curl -X GET $SERVICE_URL/2019
 
@@ -31,10 +31,10 @@ nano app.js # comment line 4 and uncomment line 5, insert your netflix-dataset-s
 npm install
 cd ~/pet-theory/lab06/firebase-frontend
 gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1
-gcloud beta run deploy frontend-staging-service --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1
-# Choose 1 and us-central1
+gcloud beta run deploy frontend-staging-service-789 --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1 --max-instances=1 --allow-unauthenticated
+# Choose 23 and us-central1
 
 # 6. Cloud Build Frontend Production
 gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-production:0.1
-gcloud beta run deploy frontend-production-service --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-production:0.1
-# Choose 1 and us-central1
+gcloud beta run deploy frontend-production-service-139 --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-production:0.1 --max-instances=1 --allow-unauthenticated
+# Choose 23 and us-central1
